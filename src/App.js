579f39b1737c6488aa858aaa5ece0a34919815dc
sakinfens <sakinfens@gmail.com>
2021-05-07 19:31:33 -0400

    changed api links

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import './App.css';
 
 const httpLink = createHttpLink({
-  uri: 'https://intense-shelf-73978.herokuapp.com/'
+  uri: 'https://shrouded-beyond-01375.herokuapp.com/'
+  
 });
 
 const client = new ApolloClient({
